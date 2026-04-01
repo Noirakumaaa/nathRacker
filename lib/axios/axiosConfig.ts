3784515f56979b325c9604db69e853b7ab2042d6
Noirakumaaa <hernandezlnathaniel@gmail.com>
2026-04-01 16:55:49 +0800

    Fix axios url auth

--- lib/axios/axiosConfig.ts
+++ lib/axios/axiosConfig.ts
@@ -4,7 +4,7 @@ import type { AxiosResponse, InternalAxiosRequestConfig } from "axios";
 import { queryClient } from "./../../app/root"; // ← import the instance directly
 
 const APIFETCH = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL ?? "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
